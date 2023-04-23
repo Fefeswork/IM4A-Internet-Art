14d37f5677a50406ae3704a4c3b349bf50c934f2
Fefeswork <2306258@students.wits.ac.za>
2023-04-23 12:13:42 +0200

    Create navigation.js

---
+++ JSFlder/navigation.js
@@ -0,0 +1,44 @@
+const pages = [
+    { url: "home.html", name: "Home", id: "home" },
+    { url: "Design.html", name: "Blog", id: "Design" },
+    { url: "Theory.html", name: "Design", id: "Theory" },
+    { url: "IA.html", name: "Data ", id: "internet art" },
+   
+];
+
+
+let navigationContainer = document.getElementById("nav-list");
+
+pages.forEach(element => {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+
+    link.innerText = element.name;
+
+    link.onclick= function() {
+        link.href = element.url;
+    }
+
+    link.id = element.id;
+   
+    if (element.id == "data-vis") {
+        const button = document.createElement("div");
+        button.id = "data-vis-button";
+        button.append(link);
+        listItem.append(button);
+    }else{
+        listItem.append(link);
+    }
+    
+    if (document.title == element.name) {
+        link.classList.add("active");
+
+        const line = document.createElement("div");
+        line.classList.add("underline");
+
+        listItem.append(line);
+    }
+
+    navigationContainer.append(listItem); 
+  
+});
